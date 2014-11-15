3ffdd0ee2fa222dccb4e0613080d619028e93d9e
polyphemus11 <miguel.zapata27@gmail.com>
2014-11-15 08:56:24 -0600

    Update google_curl.sh

--- google_curl.sh
+++ google_curl.sh
@@ -5,13 +5,14 @@ echo "What domain would you like to expand?"
 read domain
 echo "Enter the starting page (0 for 1st page)"
 read count
+count=$((count*10))
 echo "How many search result pages?"
 echo "(last search page; 10 results per page)"
 read total_pages
 total_pages=$((total_pages*10))
 while [ $count -lt $total_pages ]; do
 echo "https://www.google.de/search?tbs=li:1&q=allinurl:+-www+site:$domain&start=$count"
-curl -A "Mozilla/4.0" -skLm 10 "https://www.google.de/search?tbs=li:1&q=allinurl:+-www+site:utica.edu&start=$count" | grep -oP '\/url\?q=.+?&amp' | sed 's|/url?q=||; s|&amp||' >> curl.txt
+curl -A "Mozilla/4.0" -skLm 10 "https://www.google.com/search?tbs=li:1&q=allinurl:+-www+site:$domain&start=$count" | grep -oP '\/url\?q=.+?&amp' | sed 's|/url?q=||; s|&amp||' >> curl.txt
 count=$((count+10))
 done
 cat curl.txt | cut -d/ -f3 | sort | uniq > subdomains.txt
