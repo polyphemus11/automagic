a60d1f8efea175777d909d491eb7ff051489974c
polyphemus11 <miguel.zapata27@gmail.com>
2015-07-09 20:53:01 -0500

    Update google_curl.sh

--- google_curl.sh
+++ google_curl.sh
@@ -12,7 +12,7 @@ read total_pages
 total_pages=$((total_pages*10))
 while [ $count -lt $total_pages ]; do
 echo "https://www.google.com/search?tbs=li:1&q=allinurl:+-www+site:$domain&start=$count"
-curl -A "Mozilla/4.0" -skLm 10 "https://www.google.com/search?tbs=li:1&q=allinurl:+-www+site:$domain&start=$count" | grep -oP '\/url\?q=.+?&amp' | sed 's|/url?q=||; s|&amp||' >> curl.txt
+curl -A "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36" -skLm 10 "https://www.google.com/search?tbs=li:1&q=allinurl:+-www+site:$domain&start=$count" | grep -oP '\/url\?q=.+?&amp' | sed 's|/url?q=||; s|&amp||' >> curl.txt
 count=$((count+10))
 done
 cat curl.txt | cut -d/ -f3 | sort | uniq > subdomains.txt
