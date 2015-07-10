b3d874ebd4401147017a21752c15102fb6fe15a0
polyphemus11 <miguel.zapata27@gmail.com>
2015-07-09 20:58:04 -0500

    Update google_curl.sh

--- google_curl.sh
+++ google_curl.sh
@@ -1,6 +1,7 @@
 total_pages=0
 count=0
 domain=""
+rm URLs.txt
 echo "What domain would you like to expand?"
 read domain
 echo "Enter the starting page (0 for 1st page)"
@@ -12,10 +13,10 @@ read total_pages
 total_pages=$((total_pages*10))
 while [ $count -lt $total_pages ]; do
 echo "https://www.google.com/search?tbs=li:1&q=allinurl:+-www+site:$domain&start=$count"
-curl -A "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36" -skLm 10 "https://www.google.com/search?tbs=li:1&q=allinurl:+-www+site:$domain&start=$count" | grep -oP '\/url\?q=.+?&amp' | sed 's|/url?q=||; s|&amp||' >> curl.txt
+curl -A "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36" -skLm 10 "https://www.google.com/search?tbs=li:1&q=allinurl:+-www+site:$domain&start=$count" | grep -oP '\/url\?q=.+?&amp' | sed 's|/url?q=||; s|&amp||' >> URLs.txt
 count=$((count+10))
 done
 cat curl.txt | cut -d/ -f3 | sort | uniq > subdomains.txt
-echo "All (sub)domains found are in curl.txt"
+echo "All URLs found are in curl.txt"
 echo "All unique (sub)domains are in subdomains.txt"
 cat subdomains.txt
