8b618bc177e46473b431fd7557aa2c8dcbb52608
polyphemus11 <miguel.zapata27@gmail.com>
2015-08-05 22:55:28 -0500

    Update icmp-rx.sh

--- icmp-rx.sh
+++ icmp-rx.sh
@@ -1,11 +1,12 @@
 #$1 - sending IP
 #$2 - signature
 #$3 - interface
-#$4 - file to save as
+#$4 - plain text file to save as
 #Usage: hping3 [sender ip] --listen [signature] -I [interface] > [file]
 hping3 $1 --listen $2 -I $3 > ./.temp1
 trap " " INT
 echo "past trap"
 cut -f1 -d '=' ./.temp1 | tr -d '\n' > ./.temp2 
-echo -n '==' | cat ./.temp2 - > $4
+echo -n '==' | cat ./.temp2 - > ./exfil.base64
+base64 -d ./exfil.base64 > $4
 rm ./.temp2 & rm ./.temp1
