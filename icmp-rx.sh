b825337555cef064f12218960ffd5b0566e7f0bf
polyphemus11 <miguel.zapata27@gmail.com>
2015-09-27 16:10:13 -0500

    Update icmp-rx.sh
    removing base64 decoding

--- icmp-rx.sh
+++ icmp-rx.sh
@@ -18,8 +18,10 @@ exit;;
 done
 hping3 $1 --listen $2 -I $3 > ./.temp1
 trap " " INT
-echo "past trap"
-cut -f1 -d '=' ./.temp1 | tr -d '\n' > ./.temp2
-echo -n '==' | cat ./.temp2 - > ./exfil.base64
-base64 -d ./exfil.base64 > $4
-rm ./.temp2 & rm ./.temp1
+echo "passed trap"
+#cut -f1 -d '=' ./.temp1 | tr -d '\n' > ./.temp2
+#echo -n '==' | cat ./.temp2 - > ./exfil.base64
+#base64 -d ./exfil.base64 > $4
+#rm ./.temp2 & rm ./.temp1
+mv ./.temp1 ./$4
+rm ./.temp1
