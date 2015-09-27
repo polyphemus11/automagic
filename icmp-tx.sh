9d3181a171dbd5821c9b877e9cf650af2b170ee7
polyphemus11 <miguel.zapata27@gmail.com>
2015-09-26 20:36:43 -0500

    Update icmp-tx.sh

--- icmp-tx.sh
+++ icmp-tx.sh
@@ -1,15 +1,14 @@
-#$1 - destination IP
-#$2 - signature
-#$3 - plain text file
-#$4 - data frame size
-#$5 - icmp type
+#!/bin/bash
+#use the '-h' option to view the help (syntax) file
 #Option -C 11 is ttl exceeded preventing a reply
 #Usage: hping3 [dest ip] [signature] [tx file] [data frame size] [icmp type]
-while getopts h option
+while getopts hc option
 do
         case "${option}"
         in
                 h) echo "
+#-h - this help mess
+#-c - display a list of ICMP codes
 #\$1 - destination IP
 #\$2 - signature
 #\$3 - plain text file
@@ -17,6 +16,47 @@ do
 #\$5 - icmp type (try 11 - ttl exceeded)
 #Usage: hping3 [dest ip] [signature] [tx file] [data frame size] [icmp type]
 "
+exit;;
+                c) echo "
+ICMP TYPE NUMBERS
+
+The Internet Control Message Protocol (ICMP) has many messages that
+are identified by a "type" field.
+
+Type    Name                                    Reference
+----    -------------------------               ---------
+  0     Echo Reply                               [RFC792]
+  1     Unassigned                                  [JBP]
+  2     Unassigned                                  [JBP]
+  3     Destination Unreachable                  [RFC792]
+  4     Source Quench                            [RFC792]
+  5     Redirect                                 [RFC792]
+  6     Alternate Host Address                      [JBP]
+  7     Unassigned                                  [JBP]
+  8     Echo                                     [RFC792]
+  9     Router Advertisement                    [RFC1256]
+ 10     Router Selection                        [RFC1256]
+ 11     Time Exceeded                            [RFC792]
+ 12     Parameter Problem                        [RFC792]
+ 13     Timestamp                                [RFC792]
+ 14     Timestamp Reply                          [RFC792]
+ 15     Information Request                      [RFC792]
+ 16     Information Reply                        [RFC792]
+ 17     Address Mask Request                     [RFC950]
+ 18     Address Mask Reply                       [RFC950]
+ 19     Reserved (for Security)                    [Solo]
+ 20-29  Reserved (for Robustness Experiment)        [ZSu]
+ 30     Traceroute                              [RFC1393]
+ 31     Datagram Conversion Error               [RFC1475]
+ 32     Mobile Host Redirect              [David Johnson]
+ 33     IPv6 Where-Are-You                 [Bill Simpson]
+ 34     IPv6 I-Am-Here                     [Bill Simpson]
+ 35     Mobile Registration Request        [Bill Simpson]
+ 36     Mobile Registration Reply          [Bill Simpson]
+ 37     Domain Name Request                     [Simpson]
+ 38     Domain Name Reply                       [Simpson]
+ 39     SKIP                                    [Markson]
+ 40     Photuris                                [Simpson]"
 exit;;
         esac
 done
