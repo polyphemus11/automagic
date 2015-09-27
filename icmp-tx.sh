ecf225122262c7ede24c7107ae53e4a2bc6a8258
polyphemus11 <miguel.zapata27@gmail.com>
2015-09-26 20:05:28 -0500

    added the "-h" option
    Added the "-h" option to display the correct syntax to use

--- icmp-tx.sh
+++ icmp-tx.sh
@@ -1,10 +1,25 @@
 #$1 - destination IP
 #$2 - signature
-#$3 - plain text file 
+#$3 - plain text file
 #$4 - data frame size
 #$5 - icmp type
 #Option -C 11 is ttl exceeded preventing a reply
 #Usage: hping3 [dest ip] [signature] [tx file] [data frame size] [icmp type]
+while getopts h option
+do
+        case "${option}"
+        in
+                h) echo "
+#\$1 - destination IP
+#\$2 - signature
+#\$3 - plain text file
+#\$4 - data frame size (try 100)
+#\$5 - icmp type (try 11 - ttl exceeded)
+#Usage: hping3 [dest ip] [signature] [tx file] [data frame size] [icmp type]
+"
+exit;;
+        esac
+done
 base64 -w 0 $3 > ./.target.base64
 while true; do
         if hping3 $1 --icmp --sign $2 --file ./.target.base64 -d $4 -u -C $5 2>&1 | grep -q --line-buffered -m 1 EOF; then
